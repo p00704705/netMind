925f0567d997ef2b239a978afb26baeda78a8868
p00704705 <petermekhail01@gmail.com>
2025-06-14 10:36:11 +1000

    fixing flake8 issues

--- backend/utils/config_loader.py
+++ backend/utils/config_loader.py
@@ -1,8 +1,10 @@
 import os
 import json
 
+
 def get_config():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(base_dir, "..", "config", "tool_config.json")
     with open(config_path) as f:
         config = json.load(f)
+    return config
