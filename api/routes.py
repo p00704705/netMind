49c45fbace7c23b5b854db00bed62759fe7c119b
p00704705 <petermekhail01@gmail.com>
2025-06-21 20:00:02 +1000

    Adding new html file

--- api/routes.py
+++ api/routes.py
@@ -8,13 +8,13 @@ from backend.cache import RedisClient
 import backend.db_sql as sqldb
 from fastapi.templating import Jinja2Templates
 import os
+from backend.utils.enrich_net_data import enrich_net_data
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 router = APIRouter()
 templates = Jinja2Templates(directory=TEMPLATE_DIR)
-print(dir(templates))
 
 
 @router.get("/ping")
@@ -31,24 +31,39 @@ def help():
     return {"help": "netMind is an AI-powered network health and observability platform that helps engineers monitor network performance, detect anomalies, analyze root causes, and search operational knowledge — all from a single, unified interface."}
 
 
-@router.get("/scan")
-def scan_and_collect(subnet: str):
-    # Validate subnet format
+@router.get("/live_ip_scan", response_class=HTMLResponse)
+def scan_and_collect(request: Request, subnet: str):
+    """
+    http://0.0.0.0:4123/live_ip_scan?subnet=192.168.1.0/24
+    """
     try:
         ipaddress.ip_network(subnet)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid subnet format")
 
     scan_obj = NmapScanner([subnet]).scan_network()
-    result = {}
-    hosts = scan_obj[subnet]["hosts_ips"]
-    stats = Collector(hosts).run_test_ping()
-    result[subnet] = stats
-    return result
+    subnets_scan_obj = NmapScanner([subnet]).scan_network()
+    hosts_details = subnets_scan_obj.get(subnet).get("hosts")
+    hosts = scan_obj[subnet]["hosts"]
+    hosts_ips = [host["ip"] for host in hosts]
+    network_stats = Collector(hosts_ips).run_test_ping()
+    enriched_net_data = enrich_net_data(hosts_details, network_stats)
+    print(enriched_net_data)
+    headers = ["IP", "MAC", "Vendor", "Packet Loss (%)", "Avg Latency (ms)"]
+
+    return templates.TemplateResponse("live_scan.html", {
+        "request": request,
+        "subnet": subnet,
+        "headers": headers,
+        "rows": enriched_net_data
+    })
 
 
 @router.get("/net_stat", response_class=HTMLResponse)
 def get_net_stat(request: Request, subnet: str):
+    """
+    http://0.0.0.0:4123/net_stat?subnet=192.168.1.0/24
+    """
     try:
         ipaddress.ip_network(subnet)
     except ValueError:
